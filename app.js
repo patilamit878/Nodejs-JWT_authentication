b9cb2909e709c9a87d43060ae4365fa518931cd8
Amit Patil <patilamit878@gmail.com>
2022-02-11 12:03:25 +0530

    user registration

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
 const User = require('./models/users');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+// var crypto = require('crypto');
+// var key = "password";
+// var algo = "aes256";
 
-mongoose.connect('mongodb://localhost:27017/JWTauth',{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
+mongoose.connect('mongodb://localhost:27017/JWTauth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.warn("Database connected");
+})
+
+app.post('/register', jsonParser, function (req, res) {
+    // var cipher = crypto.createCipher(algo,key);
+    // var encrypted = cipher.update(req.body.password,'utf8','hex')+cipher.final('hex');
+    // console.warn(encrypted);
+    const data = new User({
+        name:req.body.name,
+        email:req.body.email,
+        address:req.body.address,
+        password:req.body.password,
+    })
+    data.save().then((result)=>{
+        res.json(result)
+    }).catch((err) =>{
+        console.warn(err);
+    })
 })
 
 
